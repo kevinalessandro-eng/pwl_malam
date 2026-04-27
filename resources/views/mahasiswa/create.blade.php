ea5d226602f5cbd4368f412fc06cb386838a9e8e
KevinAlessandro11 <bonfilio1112@gmail.com>
2026-04-27 14:36:02 +0700

    .

--- resources/views/mahasiswa/create.blade.php
+++ resources/views/mahasiswa/create.blade.php
@@ -3,55 +3,116 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Halaman Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1 class="text-center">Tambah Mahasiswa</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
     
-    <form class="form" class="mb-3" action="{{ action([App\Http\Controllers\MahasiswaController::class, 'store']) }}" method="post">
+    <style>
+        html, body {
+             height: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+      
+        .navbar {
+            padding: 20px; 
+        }
+
+        td {
+          font-size: 25px;
+        }
+    </style>
+</head>
+
+<body>
+  <nav class="navbar justify-content-end navbar-expand-lg bg-dark p-3 fs-4">
+<ul class="nav nav-underline">
+  <li class="nav-item">
+    <a class="nav-link link-light active" aria-current="page" href="{{ action([App\Http\Controllers\MahasiswaController::class, 'index']) }}">Mahasiswa</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\DosenController::class, 'index']) }}">Dosen</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\JurusanController::class, 'index']) }}">Jurusan</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\MataKuliahController::class, 'index']) }}">Mata Kuliah</a>
+  </li>
+</ul>
+</nav>
+
+    <h1 class="text-center p-3">Tambah Mahasiswa</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+ 
+    
+  <form class="form mb-3" action="{{ action([App\Http\Controllers\MahasiswaController::class, 'store']) }}" method="post">
         @csrf
         <table>
-            <div class="form-group">
-            <label class="form-label">Nama Lengkap :</label>
-            <input type="text" name="fullname" size="40">
-            <br>
-            </div>
+        <tr>
+            <td>Nama Lengkap</td><td>:</td><td><input type="text" name="fullname"></td>
+        </tr>
+        <tr>
+            <td>Nomor Induk Mahasiswa</td><td>:</td><td><input type="text" name="NIM"></td>
+        </tr>
+        <tr>
+            <td>Nomor Induk Siswa Nasional</td><td>:</td><td><input type="text" name="NIDN"></td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td><td>:</td><td><input type="text" name="tempat_lahir"></td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td><td>:</td><td><input type="text" name="tanggal_lahir"></td>
+        </tr>
+        <tr>
+            <td>Alamat</td><td>:</td><td><textarea name="alamat"></textarea></td>
+        </tr>
+        <tr>
+          <td>
+          <button type="submit" class="btn btn-primary btn-lg" class="button">Add</button>
+          <button type="reset" class="btn btn-secondary btn-lg" class="button">Clear</button>
+</td>
+        </tr>
+    </table>      
+  </form>
+</body>
 
-            <div class="form-group">
-            <label>Nomor Induk Mahasiswa :</label>
-            <input type="text" name="NIM" size="40">
-            <br>
-            </div>
 
-            <div class="form-group">
-            <label>Nomor Induk Siswa Nasional :</label>
-            <input type="text" name="NIDN" size="40">
-            <br>
-            </div>
+<footer class="bg-dark text-white pt-5 pb-4 mt-auto">
+  <div class="container text-center text-md-start">
+    <div class="row text-center text-md-start">
+      
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">ITBSS</h5>
+        <p>Sistem Informasi Akademik untuk pengelolaan data Mahasiswa, Dosen, Jurusan, dan Mata Kuliah.</p>
+      </div>
 
-            <div class="form-group">
-            <label>Tempat Lahir :</label>
-            <input type="text" name="tempat_lahir" size="40">
-            <br>
-            </div>
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">Menu</h5>
+        <p><a href="{{ action([App\Http\Controllers\MahasiswaController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Mahasiswa</a></p>
+        <p><a href="{{ action([App\Http\Controllers\DosenController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Dosen</a></p>
+        <p><a href="{{ action([App\Http\Controllers\JurusanController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Jurusan</a></p>
+        <p><a href="{{ action([App\Http\Controllers\MataKuliahController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Mata Kuliah</a></p>
+      </div>
 
-            <div class="form-group">
-            <label>Tanggal Lahir :</label>
-            <input type="text" name="tanggal_lahir" size="40">
-            <br>
-            </div>
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">Kontak</h5>
+        <p><i class="fas fa-home mr-3"></i> Pontianak, Indonesia</p>
+        <p><i class="fas fa-envelope mr-3"></i> admin@itbss.ac.id</p>
+      </div>
+      
+    </div>
 
-            <div class="form-group">
-            <label>Alamat :</label>
-            <textarea type="text" name="alamat" size="40"></textarea>
-            <br>
-            </div>
+    <hr class="mb-4">
 
-            <button type="submit" class="btn btn-primary btn-lg" class="button">Add</button>
-            <button type="reset" class="btn btn-secondary btn-lg" class="button">Clear</button>
-        </table>
-    </form>
+    <div class="row align-items-center">
+      <div class="col-md-7 col-lg-8">
+        <p>© 2026 Copyright: <strong>Sistem Akademik</strong></p>
+      </div>
+    </div>
+  </div>
+</footer>
+
 </html>
--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -3,56 +3,115 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Halaman Mahasiswa</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1 class="text-center">Ubah Data Mahasiswa</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
     
-    <form class="form" class="mb-3" action="{{ action([App\Http\Controllers\MahasiswaController::class, 'update'], [$mahasiswa->id]) }}" method="post">
+    <style>
+        html, body {
+             height: 100%;
+        }
+
+        body {
+            display: flex;
+            flex-direction: column;
+        }
+      
+        .navbar {
+            padding: 20px; 
+        }
+
+        td {
+          font-size: 25px;
+        }
+    </style>
+</head>
+  
+<body>
+  <nav class="navbar justify-content-end navbar-expand-lg bg-dark p-3 fs-4">
+<ul class="nav nav-underline">
+  <li class="nav-item">
+    <a class="nav-link link-light active" aria-current="page" href="{{ action([App\Http\Controllers\MahasiswaController::class, 'index']) }}">Mahasiswa</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\DosenController::class, 'index']) }}">Dosen</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\JurusanController::class, 'index']) }}">Jurusan</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link link-light" href="{{ action([App\Http\Controllers\MataKuliahController::class, 'index']) }}">Mata Kuliah</a>
+  </li>
+</ul>
+</nav>
+
+    <h1 class="text-center p-3">Ubah Data Mahasiswa</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+<form class="form" class="mb-3" action="{{ action([App\Http\Controllers\MahasiswaController::class, 'update'], [$mahasiswa->id]) }}" method="post">
         @csrf
-        <input type="hidden" name="_method" value="PUT">
+        <input type="hidden" name="_method" value="PUT">~
         <table>
-            <div class="form-group">
-            <label>Nama Lengkap :</label>
-            <input type="text" name="fullname" value="{{$mahasiswa->fullname}}" size="40">
-            <br>
-            </div>
+        <tr>
+            <td>Nama Lengkap</td><td>:</td><td><input type="text" name="fullname" value="{{$mahasiswa->fullname}}"></td>
+        </tr>
+        <tr>
+            <td>Nomor Induk Mahasiswa</td><td>:</td><td><input type="text" name="NIM" value="{{$mahasiswa->NIM}}"></td>
+        </tr>
+        <tr>
+            <td>Nomor Induk Siswa Nasional</td><td>:</td><td><input type="text" name="NIDN" value="{{$mahasiswa->NIDN}}"></td>
+        </tr>
+        <tr>
+            <td>Tempat Lahir</td><td>:</td><td><input type="text" name="tempat_lahir" value="{{$mahasiswa->tempat_lahir}}"></td>
+        </tr>
+        <tr>
+            <td>Tanggal Lahir</td><td>:</td><td><input type="text" name="tanggal_lahir" value="{{$mahasiswa->tanggal_lahir}}"></td> 
+        </tr>
+        <tr>
+            <td>Alamat</td><td>:</td><td><textarea name="alamat">{{$mahasiswa->alamat}}</textarea></td>
+        </tr>
+        <tr>
+          <td>
+          <button type="submit" class="btn btn-primary btn-lg" class="button">Add</button>
+          <button type="reset" class="btn btn-secondary btn-lg" class="button">Clear</button>
+          </td>
+        </tr>
+    </table>
+</form>
+</body>
 
-            <div class="form-group">
-            <label>Nomor Induk Mahasiswa :</label>
-            <input type="text" name="NIM" value="{{$mahasiswa->NIM}}" size="40">
-            <br>
-            </div>
+<footer class="bg-dark text-white pt-5 pb-4 mt-auto">
+  <div class="container text-center text-md-start">
+    <div class="row text-center text-md-start">
+      
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">ITBSS</h5>
+        <p>Sistem Informasi Akademik untuk pengelolaan data Mahasiswa, Dosen, Jurusan, dan Mata Kuliah.</p>
+      </div>
 
-            <div class="form-group">
-            <label>Nomor Induk Siswa Nasional :</label>
-            <input type="text" name="NIDN" value="{{$mahasiswa->NIDN}}" size="40">
-            <br>
-            </div>
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">Menu</h5>
+        <p><a href="{{ action([App\Http\Controllers\MahasiswaController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Mahasiswa</a></p>
+        <p><a href="{{ action([App\Http\Controllers\DosenController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Dosen</a></p>
+        <p><a href="{{ action([App\Http\Controllers\JurusanController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Jurusan</a></p>
+        <p><a href="{{ action([App\Http\Controllers\MataKuliahController::class, 'index']) }}" class="text-white" style="text-decoration: none;">Mata Kuliah</a></p>
+      </div>
 
-            <div class="form-group">
-            <label>Tempat Lahir :</label>
-            <input type="text" name="tempat_lahir" value="{{$mahasiswa->tempat_lahir}}" size="40">
-            <br>
-            </div>
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h5 class="text-uppercase mb-4 font-weight-bold text-info">Kontak</h5>
+        <p><i class="fas fa-home mr-3"></i> Pontianak, Indonesia</p>
+        <p><i class="fas fa-envelope mr-3"></i> admin@itbss.ac.id</p>
+      </div>
+      
+    </div>
 
-            <div class="form-group">
-            <label>Tanggal Lahir :</label>
-            <input type="text" name="tanggal_lahir" value="{{$mahasiswa->tanggal_lahir}}" size="40">
-            <br>
-            </div>
+    <hr class="mb-4">
 
-            <div class="form-group">
-            <label>Alamat :</label>
-            <textarea type="text" name="alamat" size="40">{{$mahasiswa->alamat}}</textarea>
-            <br>
-            </div>
+    <div class="row align-items-center">
+      <div class="col-md-7 col-lg-8">
+        <p>© 2026 Copyright: <strong>Sistem Akademik</strong></p>
+      </div>
+    </div>
+  </div>
+</footer>
 
-            <button type="submit" class="btn btn-primary btn-lg" class="button">Add</button>
-            <button type="reset" class="btn btn-secondary btn-lg" class="button">Clear</button>
-        </table>
-    </form>
 </html>
